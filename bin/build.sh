e725b15fa03e4c2d4ad3211f3da677961e84e08a
schizoid-cat <111165019+schizoid-cat@users.noreply.github.com>
2022-08-12 21:30:20 -0700

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -3,4 +3,24 @@ set -eu
 
 mkdir -p _site
 
+# build index
+# ===========
+
 lowdown -s -Thtml -o _site/index.html
+
+# build each page
+# ===============
+
+
+# upload
+# ======
+
+if [ "${PUBLISH:-0}" = "1" ]
+then
+  git init
+  git config user.email "${ACTOR}@users.noreply.github.com"
+  git config user.name "${ACTOR}"
+  git add -A
+  git commit -m "deploy ${GITHUB_SHA}"
+  git push -f "https://${ACTOR}:${TOKEN}@github.com/${REPO}.git" main:${BRANCH}
+fi
